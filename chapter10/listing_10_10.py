2905f24da151309ceb84c2a7b9f7a1c1a6342f60
manuelrubio <manuel.rubio@urjc.es>
2023-12-21 16:58:08 +0100

    Delete chapter10 directory

--- chapter10/listing_10_10.py
+++
@@ -1,6 +0,0 @@
-# Preconditions: n>=m, n>=2, m>=2
-def smallest_prime_factor(n, m):
-    if n % m == 0:
-        return m
-    else:
-        return smallest_prime_factor(n, m + 1)
--- chapter10/listing_10_11.py
+++
@@ -1,5 +0,0 @@
-def floor_log(x, b):
-    if x == 1:
-        return 0
-    else:
-        return 1 + floor_log(x / b, b)
