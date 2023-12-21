29eb3d140208ac4d69b83258f5ed1b65a5cbbd6b
manuelrubio <manuel.rubio@urjc.es>
2023-12-21 16:58:01 +0100

    Delete chapter09 directory

--- chapter09/listing_09_13.py
+++
@@ -1,5 +0,0 @@
-def parenthesized_expression(tokens):
-    if tokens == [] or tokens[0] != '(' or tokens[-1] != ')':
-        raise SyntaxError('Syntax error')
-    else:
-        return expression(tokens[1:-1])
